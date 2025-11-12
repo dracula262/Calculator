6e9d19a56540ce7c949844ab6f02c7a4235645a4
Gabriel Draki <drakigabriel@gmail.com>
2025-11-12 01:05:22 -0300

    #add Layout Done

---
+++ Calculator/Main.Designer.cs
@@ -0,0 +1,419 @@
+﻿namespace Calculator
+{
+    partial class frmCalculadora
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            txtHistory = new TextBox();
+            txtVisor = new TextBox();
+            btn6 = new Button();
+            btn9 = new Button();
+            btn4 = new Button();
+            btn1 = new Button();
+            btn5 = new Button();
+            btnPlus = new Button();
+            btn2 = new Button();
+            btn0 = new Button();
+            btnMinus = new Button();
+            btnDiv = new Button();
+            btnMulti = new Button();
+            btnRoot = new Button();
+            btnPercent = new Button();
+            btnDelete = new Button();
+            btnClear = new Button();
+            btnNegate = new Button();
+            btnEqual = new Button();
+            btn7 = new Button();
+            btnCollon = new Button();
+            btn3 = new Button();
+            btn8 = new Button();
+            btnClearH = new Button();
+            SuspendLayout();
+            // 
+            // txtHistory
+            // 
+            txtHistory.BorderStyle = BorderStyle.None;
+            txtHistory.Enabled = false;
+            txtHistory.Font = new Font("Microsoft YaHei UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txtHistory.ForeColor = SystemColors.WindowText;
+            txtHistory.Location = new Point(12, 48);
+            txtHistory.Name = "txtHistory";
+            txtHistory.ReadOnly = true;
+            txtHistory.RightToLeft = RightToLeft.Yes;
+            txtHistory.Size = new Size(373, 25);
+            txtHistory.TabIndex = 0;
+            txtHistory.TabStop = false;
+            txtHistory.Text = "1+1";
+            // 
+            // txtVisor
+            // 
+            txtVisor.BorderStyle = BorderStyle.None;
+            txtVisor.Font = new Font("Microsoft YaHei UI", 36F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            txtVisor.ImeMode = ImeMode.On;
+            txtVisor.Location = new Point(12, 70);
+            txtVisor.Name = "txtVisor";
+            txtVisor.ReadOnly = true;
+            txtVisor.RightToLeft = RightToLeft.Yes;
+            txtVisor.Size = new Size(373, 61);
+            txtVisor.TabIndex = 1;
+            txtVisor.TabStop = false;
+            txtVisor.Text = "0";
+            // 
+            // btn6
+            // 
+            btn6.BackColor = SystemColors.HighlightText;
+            btn6.FlatStyle = FlatStyle.System;
+            btn6.Font = new Font("Microsoft YaHei UI", 14.25F);
+            btn6.Location = new Point(156, 328);
+            btn6.Name = "btn6";
+            btn6.Size = new Size(70, 70);
+            btn6.TabIndex = 19;
+            btn6.Text = "6";
+            btn6.UseVisualStyleBackColor = false;
+            // 
+            // btn9
+            // 
+            btn9.BackColor = SystemColors.HighlightText;
+            btn9.FlatStyle = FlatStyle.System;
+            btn9.Font = new Font("Microsoft YaHei UI", 14.25F);
+            btn9.Location = new Point(156, 256);
+            btn9.Name = "btn9";
+            btn9.Size = new Size(70, 70);
+            btn9.TabIndex = 22;
+            btn9.Text = "9";
+            btn9.UseVisualStyleBackColor = false;
+            // 
+            // btn4
+            // 
+            btn4.BackColor = SystemColors.HighlightText;
+            btn4.FlatStyle = FlatStyle.System;
+            btn4.Font = new Font("Microsoft YaHei UI", 14.25F);
+            btn4.Location = new Point(12, 329);
+            btn4.Name = "btn4";
+            btn4.Size = new Size(70, 70);
+            btn4.TabIndex = 17;
+            btn4.Text = "4";
+            btn4.UseVisualStyleBackColor = false;
+            // 
+            // btn1
+            // 
+            btn1.BackColor = SystemColors.HighlightText;
+            btn1.FlatStyle = FlatStyle.System;
+            btn1.Font = new Font("Microsoft YaHei UI", 14.25F);
+            btn1.Location = new Point(12, 402);
+            btn1.Name = "btn1";
+            btn1.Size = new Size(70, 70);
+            btn1.TabIndex = 14;
+            btn1.Text = "1";
+            btn1.UseVisualStyleBackColor = false;
+            // 
+            // btn5
+            // 
+            btn5.BackColor = SystemColors.HighlightText;
+            btn5.FlatStyle = FlatStyle.System;
+            btn5.Font = new Font("Microsoft YaHei UI", 14.25F);
+            btn5.Location = new Point(84, 328);
+            btn5.Name = "btn5";
+            btn5.Size = new Size(70, 70);
+            btn5.TabIndex = 18;
+            btn5.Text = "5";
+            btn5.UseVisualStyleBackColor = false;
+            // 
+            // btnPlus
+            // 
+            btnPlus.BackColor = SystemColors.HighlightText;
+            btnPlus.FlatStyle = FlatStyle.System;
+            btnPlus.Font = new Font("Microsoft YaHei UI", 24F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnPlus.Location = new Point(232, 402);
+            btnPlus.Name = "btnPlus";
+            btnPlus.Size = new Size(75, 70);
+            btnPlus.TabIndex = 10;
+            btnPlus.Text = "+";
+            btnPlus.UseVisualStyleBackColor = false;
+            // 
+            // btn2
+            // 
+            btn2.BackColor = SystemColors.HighlightText;
+            btn2.FlatStyle = FlatStyle.System;
+            btn2.Font = new Font("Microsoft YaHei UI", 14.25F);
+            btn2.Location = new Point(84, 402);
+            btn2.Name = "btn2";
+            btn2.Size = new Size(70, 70);
+            btn2.TabIndex = 15;
+            btn2.Text = "2";
+            btn2.UseVisualStyleBackColor = false;
+            // 
+            // btn0
+            // 
+            btn0.BackColor = SystemColors.HighlightText;
+            btn0.FlatStyle = FlatStyle.System;
+            btn0.Font = new Font("Microsoft YaHei UI", 14.25F);
+            btn0.Location = new Point(12, 475);
+            btn0.Name = "btn0";
+            btn0.Size = new Size(142, 70);
+            btn0.TabIndex = 13;
+            btn0.Text = "0";
+            btn0.UseVisualStyleBackColor = false;
+            // 
+            // btnMinus
+            // 
+            btnMinus.BackColor = SystemColors.HighlightText;
+            btnMinus.FlatStyle = FlatStyle.System;
+            btnMinus.Font = new Font("Microsoft YaHei UI", 24F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnMinus.Location = new Point(310, 402);
+            btnMinus.Name = "btnMinus";
+            btnMinus.Size = new Size(75, 70);
+            btnMinus.TabIndex = 11;
+            btnMinus.Text = "-";
+            btnMinus.UseVisualStyleBackColor = false;
+            // 
+            // btnDiv
+            // 
+            btnDiv.BackColor = SystemColors.HighlightText;
+            btnDiv.FlatStyle = FlatStyle.System;
+            btnDiv.Font = new Font("Microsoft YaHei UI", 24F);
+            btnDiv.Location = new Point(310, 329);
+            btnDiv.Name = "btnDiv";
+            btnDiv.Size = new Size(75, 70);
+            btnDiv.TabIndex = 9;
+            btnDiv.Text = "÷";
+            btnDiv.UseVisualStyleBackColor = false;
+            // 
+            // btnMulti
+            // 
+            btnMulti.BackColor = SystemColors.HighlightText;
+            btnMulti.FlatStyle = FlatStyle.System;
+            btnMulti.Font = new Font("Microsoft YaHei UI", 24F);
+            btnMulti.Location = new Point(232, 329);
+            btnMulti.Name = "btnMulti";
+            btnMulti.Size = new Size(75, 70);
+            btnMulti.TabIndex = 8;
+            btnMulti.Text = "×";
+            btnMulti.UseVisualStyleBackColor = false;
+            // 
+            // btnRoot
+            // 
+            btnRoot.BackColor = SystemColors.HighlightText;
+            btnRoot.FlatStyle = FlatStyle.System;
+            btnRoot.Font = new Font("Microsoft YaHei UI", 24F);
+            btnRoot.Location = new Point(310, 256);
+            btnRoot.Name = "btnRoot";
+            btnRoot.Size = new Size(75, 70);
+            btnRoot.TabIndex = 7;
+            btnRoot.Text = "√";
+            btnRoot.UseVisualStyleBackColor = false;
+            // 
+            // btnPercent
+            // 
+            btnPercent.BackColor = SystemColors.HighlightText;
+            btnPercent.FlatStyle = FlatStyle.System;
+            btnPercent.Font = new Font("Microsoft YaHei UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnPercent.Location = new Point(232, 256);
+            btnPercent.Name = "btnPercent";
+            btnPercent.Size = new Size(75, 70);
+            btnPercent.TabIndex = 6;
+            btnPercent.Text = "%";
+            btnPercent.UseVisualStyleBackColor = false;
+            // 
+            // btnDelete
+            // 
+            btnDelete.BackColor = SystemColors.HighlightText;
+            btnDelete.FlatStyle = FlatStyle.System;
+            btnDelete.Font = new Font("Microsoft YaHei UI", 27.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnDelete.Location = new Point(232, 180);
+            btnDelete.Name = "btnDelete";
+            btnDelete.Size = new Size(153, 70);
+            btnDelete.TabIndex = 5;
+            btnDelete.Text = "←";
+            btnDelete.UseVisualStyleBackColor = false;
+            // 
+            // btnClear
+            // 
+            btnClear.BackColor = SystemColors.HighlightText;
+            btnClear.FlatStyle = FlatStyle.System;
+            btnClear.Font = new Font("Microsoft YaHei UI", 15.75F);
+            btnClear.Location = new Point(156, 180);
+            btnClear.Name = "btnClear";
+            btnClear.Size = new Size(70, 70);
+            btnClear.TabIndex = 4;
+            btnClear.Text = "C";
+            btnClear.UseVisualStyleBackColor = false;
+            // 
+            // btnNegate
+            // 
+            btnNegate.BackColor = SystemColors.HighlightText;
+            btnNegate.FlatStyle = FlatStyle.System;
+            btnNegate.Font = new Font("Microsoft YaHei UI", 14.25F);
+            btnNegate.Location = new Point(12, 180);
+            btnNegate.Name = "btnNegate";
+            btnNegate.Size = new Size(70, 70);
+            btnNegate.TabIndex = 2;
+            btnNegate.Text = "+/-";
+            btnNegate.UseVisualStyleBackColor = false;
+            // 
+            // btnEqual
+            // 
+            btnEqual.BackColor = SystemColors.HotTrack;
+            btnEqual.BackgroundImageLayout = ImageLayout.None;
+            btnEqual.FlatAppearance.BorderSize = 0;
+            btnEqual.FlatStyle = FlatStyle.Flat;
+            btnEqual.Font = new Font("Microsoft YaHei UI", 27.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnEqual.ForeColor = SystemColors.ButtonHighlight;
+            btnEqual.Location = new Point(232, 475);
+            btnEqual.Name = "btnEqual";
+            btnEqual.Size = new Size(153, 70);
+            btnEqual.TabIndex = 12;
+            btnEqual.Text = "=";
+            btnEqual.UseVisualStyleBackColor = false;
+            // 
+            // btn7
+            // 
+            btn7.BackColor = SystemColors.HighlightText;
+            btn7.FlatStyle = FlatStyle.System;
+            btn7.Font = new Font("Microsoft YaHei UI", 14.25F);
+            btn7.Location = new Point(12, 256);
+            btn7.Name = "btn7";
+            btn7.Size = new Size(70, 70);
+            btn7.TabIndex = 20;
+            btn7.Text = "7";
+            btn7.UseVisualStyleBackColor = false;
+            // 
+            // btnCollon
+            // 
+            btnCollon.BackColor = SystemColors.HighlightText;
+            btnCollon.FlatStyle = FlatStyle.System;
+            btnCollon.Font = new Font("Microsoft YaHei UI", 14.25F);
+            btnCollon.Location = new Point(156, 475);
+            btnCollon.Name = "btnCollon";
+            btnCollon.Size = new Size(70, 70);
+            btnCollon.TabIndex = 23;
+            btnCollon.Text = ",";
+            btnCollon.UseVisualStyleBackColor = false;
+            // 
+            // btn3
+            // 
+            btn3.BackColor = SystemColors.HighlightText;
+            btn3.FlatStyle = FlatStyle.System;
+            btn3.Font = new Font("Microsoft YaHei UI", 14.25F);
+            btn3.Location = new Point(156, 402);
+            btn3.Name = "btn3";
+            btn3.Size = new Size(70, 70);
+            btn3.TabIndex = 16;
+            btn3.Text = "3";
+            btn3.UseVisualStyleBackColor = false;
+            // 
+            // btn8
+            // 
+            btn8.BackColor = SystemColors.HighlightText;
+            btn8.FlatStyle = FlatStyle.System;
+            btn8.Font = new Font("Microsoft YaHei UI", 14.25F);
+            btn8.Location = new Point(84, 256);
+            btn8.Name = "btn8";
+            btn8.Size = new Size(70, 70);
+            btn8.TabIndex = 21;
+            btn8.Text = "8";
+            btn8.UseVisualStyleBackColor = false;
+            // 
+            // btnClearH
+            // 
+            btnClearH.BackColor = SystemColors.HighlightText;
+            btnClearH.FlatStyle = FlatStyle.System;
+            btnClearH.Font = new Font("Microsoft YaHei UI", 15.75F);
+            btnClearH.Location = new Point(84, 180);
+            btnClearH.Name = "btnClearH";
+            btnClearH.Size = new Size(70, 70);
+            btnClearH.TabIndex = 3;
+            btnClearH.Text = "CE";
+            btnClearH.UseVisualStyleBackColor = false;
+            // 
+            // frmCalculadora
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ControlLight;
+            ClientSize = new Size(397, 574);
+            Controls.Add(btnClearH);
+            Controls.Add(btn8);
+            Controls.Add(btn3);
+            Controls.Add(btnCollon);
+            Controls.Add(btn7);
+            Controls.Add(btnEqual);
+            Controls.Add(btnNegate);
+            Controls.Add(btnClear);
+            Controls.Add(btnDelete);
+            Controls.Add(btnRoot);
+            Controls.Add(btnPercent);
+            Controls.Add(btnDiv);
+            Controls.Add(btnMulti);
+            Controls.Add(btnMinus);
+            Controls.Add(btn0);
+            Controls.Add(btn2);
+            Controls.Add(btnPlus);
+            Controls.Add(btn5);
+            Controls.Add(btn1);
+            Controls.Add(btn4);
+            Controls.Add(btn9);
+            Controls.Add(btn6);
+            Controls.Add(txtVisor);
+            Controls.Add(txtHistory);
+            FormBorderStyle = FormBorderStyle.FixedDialog;
+            MaximizeBox = false;
+            Name = "frmCalculadora";
+            Text = "Calculadora";
+            Load += frmCalculadora_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox txtHistory;
+        private TextBox txtVisor;
+        private Button btn6;
+        private Button btn9;
+        private Button btn4;
+        private Button btn1;
+        private Button btn5;
+        private Button btnPlus;
+        private Button btn2;
+        private Button btn0;
+        private Button btnMinus;
+        private Button btnDiv;
+        private Button btnMulti;
+        private Button btnRoot;
+        private Button btnPercent;
+        private Button btnDelete;
+        private Button btnClear;
+        private Button btnNegate;
+        private Button btnEqual;
+        private Button btn7;
+        private Button btnCollon;
+        private Button btn3;
+        private Button btn8;
+        private Button btnClearH;
+    }
+}
---
+++ Calculator/Main.cs
@@ -0,0 +1,22 @@
+namespace Calculator
+{
+    public partial class frmCalculadora : Form
+    {
+        public frmCalculadora()
+        {
+            InitializeComponent();
+        }
+
+        private void frmCalculadora_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtHistórico_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+       
+    }
+}
